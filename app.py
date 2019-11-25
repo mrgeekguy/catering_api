d77c574b00368edce7fc58ca1ba5b470f7eaed75
mrgeekguy <mrgeekguymd@gmail.com>
2019-11-25 12:53:21 -0500

    Modified models

--- app.py
+++ app.py
@@ -5,6 +5,3 @@ app = Flask(__name__)
 
 if __name__ == "__main__":
     app.run() # do not use in deployment environment
-
-
-
---
+++ models/client.py
@@ -0,0 +1,24 @@
+import datetime
+from . import db
+
+class Client():
+    __tablename__ = "clients"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(500), nullable=False)
+    name = db.Column(db.String(30), nullable=False)
+    surname = db.Column(db.String(50))
+    date_created = db.Column(db.DateTime)
+    last_modified = db.Column(db.DateTime)
+
+    def __init__(self, email, password, name, surname=None):
+        self.email = email
+        self.password = password
+        self.name = name
+        self.surname = surname
+
+    def save(self):
+        db.session.add(self)
+        db.session.save()
+        db.session.commit()
